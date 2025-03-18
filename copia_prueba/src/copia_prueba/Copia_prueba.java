0d813c1e420fcce10a67f75149304e328ce45677
joshua122388 <82735801+joshua122388@users.noreply.github.com>
2025-03-17 19:52:39 -0600

    files

---
+++ copia_prueba/src/copia_prueba/Copia_prueba.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package copia_prueba;
+
+/**
+ *
+ * @author contr
+ */
+public class Copia_prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola esto es una prueba");
+            
+    }
+    
+}
